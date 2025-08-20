37a15b2638f0e16fe58126fb5d8c22deb9a27fd1
Harsh chaudhary <harshchikara408@gmail.com>
2025-08-20 17:06:40 +0530

    done with fundamentals

--- Level1/sam_average_marks.java
+++
@@ -1,9 +0,0 @@
-public class sam_average_marks {
-    public static void main(String[] args) {
-        int Maths=94;
-        int Physics=95;
-        int Chemistry=96;
-        int average=(94+95+96)/3;
-        System.out.println("Sam’s average mark in PCM is "+average);
-    }
-}
