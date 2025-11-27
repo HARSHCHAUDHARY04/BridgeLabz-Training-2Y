c8cdd2a044261eb0ee47855b53a28c1474cee520
Harsh chaudhary <harshchikara408@gmail.com>
2025-11-27 12:21:24 +0530

    Update JDBCUtil.java

--- Project/src/main/java/org/example/util/JDBCUtil.java
+++ Project/src/main/java/org/example/util/JDBCUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class JDBCUtil {
     private static final String URL = "jdbc:mysql://localhost:3306/student_db?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASS = "Harsh2480";
+    private static final String PASS = "Enter your password";
 
     static {
         try {
